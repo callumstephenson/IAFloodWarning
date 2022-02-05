593e96511a5fc5c3baac90e4a03990791a2e9d64
callumstephenson <callumstephenson03@gmail.com>
2022-02-05 17:10:18 +0000

    Completed Task1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,14 @@
 geographical data.
 
 """
-
+from haversine import haversine, Unit
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+    x = []
+    for each in stations:
+        coord = each.coord
+        dist = haversine(p,coord)
+        x.append((each,float(dist)))
+    x = sorted_by_key(x,1)
+    return x
