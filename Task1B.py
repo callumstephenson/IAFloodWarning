6b15e8f24f6e6122acac6af8cbf049b6d5bfd1e2
callumstephenson <callumstephenson03@gmail.com>
2022-02-07 16:40:18 +0000

    changed list comprehension

--- Task1B.py
+++ Task1B.py
@@ -4,7 +4,7 @@ from floodsystem.stationdata import build_station_list
 def run():
     stations = build_station_list()
     x = stations_by_distance(stations,(52.2053,0.1218))
-    y = [(x[i][0].name,x[i][0].town,x[i][1]) for i in range(len(x))]
+    y = [(station[0].name,station[0].town,station[1]) for station in x]
     print(y[:10]) , print(y[-10:])
     return None
 
