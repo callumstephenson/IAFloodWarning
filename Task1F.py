f7175e3d7445b41f0ea649af8d99e0ec408049be
JerryPang7 <98505651+JerryPang7@users.noreply.github.com>
2022-02-09 16:43:52 +0000

    fixed 1f

--- Task1F.py
+++ Task1F.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
 
@@ -15,18 +14,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-=======
-from floodsystem.stationdata import build_station_list
-from floodsystem.station import inconsistent_typical_range_stations
-
-
-def run():
-    stations = build_station_list()
-    inconsistent = inconsistent_typical_range_stations(stations)
-    inconsistent.sort()
-    print(inconsistent)
-    return None
-
-if __name__ == "__main__":
-    run()
->>>>>>> aca3281531aadc1992e9fb3c6d4e6163d176e4b7
