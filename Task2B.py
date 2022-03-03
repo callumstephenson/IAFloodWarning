45de8e735d2804df9a5e5fc2cefbc2acf661929e
JerryPang7 <98505651+JerryPang7@users.noreply.github.com>
2022-03-03 16:52:22 +0000

    changed task2b

--- Task2B.py
+++ Task2B.py
@@ -5,7 +5,8 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     x = stations_level_over_threshold(stations, 0.8)
-    print(x)
+    for each in x:
+        print(each[0].name, each[1])
     return None
 
 
