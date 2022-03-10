ee71b02d84b9b22da29b5a5a376adea2ef29071a
JerryPang7 <98505651+JerryPang7@users.noreply.github.com>
2022-03-10 10:09:40 +0000

    test geo

--- test_geo.py
+++ test_geo.py
@@ -12,26 +12,26 @@ def test_stations_by_distance():
     stations_distance = stations_by_distance(selected_stations, (52.2053, 0.1218))
     ''' check with a specific input
     '''
-    assert stations_distance[0][0].name == 'Cambridge Jesus Lock'
-    assert stations_distance[-1][0].name == 'Penberth'
-    assert stations_distance[0][1] == 0.840237595667494
+    assert stations_distance[0][0].name == 'Hemingford'
+    assert stations_distance[-1][0].name == 'A6 Bridge'
+    assert stations_distance[0][1] == 20.089233515210974
     ''' Check that the list is sorted by distance
     '''
     for i in range(len(stations_distance) - 1):
-        assert stations_distance[i][2] < stations_distance[i+1][2]
+        assert stations_distance[i][1] < stations_distance[i+1][1]
 
 def test_stations_within_radius():
     ''' Build list of stations within 10km of cambridge city centre
     '''
     stations = build_station_list()
-    stations_radius = stations_within_radius(stations.build_station_list(), (52.2053, 0.1218), 10)
+    stations_radius = stations_within_radius(stations, (52.2053, 0.1218), 10)
     ''' check with a specific input
     '''
     assert len(stations_radius) == 11
     ''' Check for all the elements
     '''
     for station in stations_radius:
-        radius = haversine(station.coord, (52.2053, 0.1218))
+        radius = station[1]
         assert radius < 10
 
 def test_rivers_with_station():
@@ -63,10 +63,10 @@ def test_rivers_by_station_number():
     station_5 = MonitoringStation(s_id, m_id, label, coord, trange, river2, town)
     stations = [station_1, station_2, station_3, station_4, station_5]
 
-    assert len(rivers_by_station_number(stations, 1)) == 1
+    assert len(rivers_by_station_number(stations, 1)) == 2
 
     river_list = rivers_by_station_number(stations, 2)
 
-    assert len(river_list) == 2
+    assert len(river_list) == 3
 
-    assert "River 3" not in [item[0] for item in river_list]
+    assert "River 3" not in [item[1] for item in river_list]
